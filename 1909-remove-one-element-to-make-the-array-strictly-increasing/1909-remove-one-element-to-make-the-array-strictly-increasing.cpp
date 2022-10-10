582a139e1ea4644ac54c5d77e85889d1d0611f82
jharaghav32 <83938321+jharaghav32@users.noreply.github.com>
2022-10-10 19:13:22 +0530

    Time: 13 ms (33.19%), Space: 9.9 MB (81.97%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -8,17 +8,13 @@ public:
         for(int i=1;i<n;i++){
          if(nums[i]<=prev){
              if(one)return false;
-
              one=true;
-
          }
         if(i!=1 && nums[i-2]>=nums[i]){
             prev=prev;
              }
             else
-         prev=nums[i];
-        
-            
+         prev=nums[i]; 
         }
         return true;
     }
