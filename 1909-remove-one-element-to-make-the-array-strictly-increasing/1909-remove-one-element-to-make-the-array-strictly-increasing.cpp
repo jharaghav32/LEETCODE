8d74a8b69d3ac3434c975f85d8c621d07b9a37d3
jharaghav32 <83938321+jharaghav32@users.noreply.github.com>
2022-10-10 18:56:20 +0530

    Time: 11 ms (44.65%), Space: 10 MB (58.97%) - LeetHub

---
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canBeIncreasing(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1)return true;
+        int prev=nums[0];
+        bool one=false;
+        for(int i=1;i<n;i++){
+         if(nums[i]<=prev){
+             if(one)return false;
+
+             one=true;
+
+         }
+        if(i!=1 && nums[i-2]>=nums[i]){
+            prev=prev;
+             }
+            else
+         prev=nums[i];
+        
+            
+        }
+        return true;
+    }
+};
