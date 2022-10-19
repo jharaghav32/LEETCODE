ae22e1507367306b8b4340563641990f2d8d13f7
RAGHAV KUMAR JHA <83938321+jharaghav32@users.noreply.github.com>
2022-10-19 11:08:08 +0530

    Time: 6 ms (70.50%), Space: 9.5 MB (78.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,7 +3,8 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
-        
+        // We start from top right element and check if it is eual to target than rteturn true
+        // if it is greater than target  then we have to find less one so we move to prev column as element in next row is obiviously greater so no need to go to next to row we only have to search in column if it exist return true and when our iterator cross limit it means the element is not found return false;
         int i=0;
         int j = n-1;
         while(j>=0 && i<m){
