236ee9a59395240e7d53511545993edc4b416887
RAGHAV KUMAR JHA <83938321+jharaghav32@users.noreply.github.com>
2022-10-22 11:17:17 +0530

    Time: 17 ms (76.73%), Space: 6 MB (31.16%) - LeetHub

--- 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.cpp
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.cpp
@@ -11,11 +11,16 @@ public:
     int findKthNumber(int m, int n, int k) {
         int low=1;
         int high=m*n;
+        // range of elment is [1,high];
         while(low<high){
             int mid = low + (high-low)/2;
-            int   smMidEle = count(mid,m,n);
-            if(smMidEle<k) low  = mid+1;
-            else high =mid;
+            int   smMidEle = count(mid,m,n); // ye calculate krta hai ki kitne element matrix 
+            // less than and eqal to mid hai 
+            if(smMidEle<k) low  = mid+1;//Agar no of element less than equal to mid is 
+            // less than k agar aisa hai to eska matlab hame mid ko badhana padega tabhi jabhi element milenge so we increase our low pointer to mid+1;
+            else high =mid;// and agar element k se jayada hogye to mid ko ghantan padega so
+            // we move our high pointer to mid  
+            //and atlast when low becomes equal to high we get the kth smallest element 
         }
         return low;
     }
