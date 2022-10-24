5b9f04ad0559e4bed16cd37176d030d3231ae13f
RAGHAV KUMAR JHA <83938321+jharaghav32@users.noreply.github.com>
2022-10-24 23:40:46 +0530

    Time: 150 ms (15.73%), Space: 69.8 MB (9.00%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -11,7 +11,7 @@ public:
         combination(candidates,sum,level,idx+1);
         level.push_back(candidates[idx]);// include the element to take that element 
         combination(candidates,sum-candidates[idx],level,idx);
-        level.pop_back();
+        level.pop_back();// backtracking remove the included element yaha pe ye option nhi bhi rakhe to bhi chalega kyoki actual me jab call kr rhe hai to labhag ye wo wla bhi case cover kr leta jisme ye curr element included nhi hai by using not take recursion call
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int>level;
