0900fc8cb798dce2a7493e9d25d2203713df5229
RAGHAV KUMAR JHA <83938321+jharaghav32@users.noreply.github.com>
2022-10-24 23:31:15 +0530

    Time: 199 ms (11.59%), Space: 69.8 MB (9.32%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void combination(vector<int>&candidates,int sum,vector<int>level,int idx){
+        if(sum==0){
+            ans.push_back(level);
+            return;
+        }
+        if(idx>=candidates.size() || sum<0 )
+            return;
+        combination(candidates,sum,level,idx+1);
+        level.push_back(candidates[idx]);
+        combination(candidates,sum-candidates[idx],level,idx);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>level;
+        combination(candidates,target,level,0);
+        return ans;
+    }
+};
