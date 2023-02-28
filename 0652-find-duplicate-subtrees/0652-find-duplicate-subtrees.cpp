17cc0729de6b93eb2d509f4095ff9a2d7b699feb
RAGHAV KUMAR JHA <83938321+jharaghav32@users.noreply.github.com>
2023-02-28 16:54:52 +0530

    Time: 24 ms (86.10%), Space: 31.8 MB (88.38%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -11,23 +11,17 @@
  */
 class Solution {
 public:
-        unordered_map<string,int>ump;
+    unordered_map<string,int>ump;
     vector<TreeNode*>ans;
-        string Serialisation(TreeNode* root){
-            if(root==NULL)return "";
-          
-         string str = "(" + Serialisation(root->left)+to_string(root->val) +  Serialisation(root->right)  +")";
-          
-           
-           
-            
-            if(ump[str]==1)ans.push_back(root);
-            ump[str]++;
-            
-            return str;
-        }
+    string serial(TreeNode* root){
+        if(root==NULL)return "";
+        string str = "("+serial(root->left)+to_string(root->val)+serial(root->right)+")";
+        if(ump[str]==1)ans.push_back(root);
+        ump[str]++;
+        return str;
+    }
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-       Serialisation(root);
+        serial(root);
         return ans;
     }
 };
