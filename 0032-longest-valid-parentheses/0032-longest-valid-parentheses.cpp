95788dcb82adda77b316f3f89eef3d6276f8e9e1
RAGHAV KUMAR JHA <83938321+jharaghav32@users.noreply.github.com>
2023-04-10 10:55:20 +0530

    Time: 4 ms (71.71%), Space: 7.4 MB (46.60%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int n = s.size();
-        vector<bool>val(n,false);
-        stack<pair<char,int>>st;
-        for(int i=0;i<n;i++){    
-         if(s[i]=='(')
-            st.push({s[i],i});
-            else if(s[i]==')' && !st.empty() && st.top().first=='(')
-            {
-               val[i]=true;
-                val[st.top().second]=true;
-                st.pop();
-            }
-          else{
-              while(!st.empty()){
-                  val[st.top().second]=false;
-                  st.pop();
-              }
-          }
-        }
-       while(!st.empty()){
-            val[st.top().second]=false;
-                  st.pop();
-       }
+      stack<int>st;
+     int n = s.size();
+        //initially put -1 to show that until now the index of invalid parenthesis doesn't known to us so insert -1;
+        st.push(-1);
         int ans=0;
-        int tempans=0;
-       for(int i=0;i<n;i++){
-           if(val[i]==true){
-               tempans++;
-           }
-           else{
-               ans = max(ans,tempans);
-               tempans=0;
-           }
-       }
-        ans=max(ans,tempans);
+        for(int i=0;i<n;i++){
+            //if char is open bracket simply insert the index into stack
+            if(s[i]=='('){
+                st.push(i);
+            }
+            // if bracket is closed then remove the top element because that one matches the open bracket if matches bracket doesn't found that means stack becomes empty  so insert the index of closed bracket into stack that gives us a find that upto that index invalid parenthesis occurs that helps us to find the length
+            else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }
+                // on removal if stack is not empty  than we can calculated the length that is i- stack top elemetn because the after the top element the parenthesis is valid and we calculate its length
+                ans = max(ans,(i-st.top()));
+            }
+        }
         return ans;
     }
+    // timecomplexity -O(n) && Space--O(n)
 };
