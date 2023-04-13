071c6a76937eb0fc8cf606ca36ca78a89df18d4a
RAGHAV KUMAR JHA <83938321+jharaghav32@users.noreply.github.com>
2023-04-13 10:49:43 +0530

    Time: 13 ms (33.03%), Space: 15.1 MB (98.01%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        stack<int>st;
-        unordered_map<int,int>ump;
-        for(int i=0;i<pushed.size();i++){
-            ump[pushed[i]]=i;
-        }
-        int mark;
-        int push=-1;
-        for(int i=0;i<popped.size();i++){
-            int elemIdx = ump[popped[i]];
-            mark = push+1;
-            while(mark<=elemIdx){
-                st.push(mark); 
-                push = mark;
-                mark++;
+        //first initialised the two pointer to zero of both array
+        int i=0;
+        int j=0;
+        //As in first approach we created a stack to take care of which element can be removed but here we considered pushed array as a stack using pointer 
+        //we traverse pushed array
+        for(int k=0;k<pushed.size();k++){
+            //push element into pushed array (as a stack)
+            pushed[i]=pushed[k];
+            //then increment i which specify whcih position now we can push next element
+            i++;
+            //after that check whether the last element of pushed array that is (i-1) is equal to popped element
+            while(i>0 && pushed[i-1]==popped[j]){
+                //remove elment using pointer  decrementing the i pointer means now we can push next elment on one less positoin and increment the j pointer to get next popped element
+                i--;
+                j++;
             }
-           if(st.top()==elemIdx){
-               st.pop();
-           }
-            else{
-                return false;
-            }
-            
         }
-        return true;
+        // if i pointer reaches 0 it means we have succesfully popped out all element so return true
+        if(i==0)return true;
+        else return false;
     }
 };
